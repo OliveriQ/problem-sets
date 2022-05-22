78577e9dda4156ce195a1813c127719367a6466f
OliveriQ <oliverilucas20@gmail.com>
2022-05-22 17:38:37 +0100

    Distinct Numbers

--- Tests/main.cpp
+++ Tests/main.cpp
@@ -2,5 +2,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
+    int n, m, k;
+    cin >> n >> m >> k;
+    vector<int> a(n);
+    vector<int> b(m);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < m; i++) {
+        cin >> b[i];
+    }
     
 }
