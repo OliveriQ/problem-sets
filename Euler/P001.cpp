9283ff1e6959f464942251c6d8dac7c15e8e8419
OliveriQ <oliverilucas20@gmail.com>
2022-06-01 21:27:15 +0100

    del euler for now

--- Euler/P001.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main() {
-    int ans = 0;
-    for (int i = 1; i < 1000; i++) {
-        ans += (i%3==0||i%5==0)?i:0;
-    }
-    cout << ans << "\n";
-}
